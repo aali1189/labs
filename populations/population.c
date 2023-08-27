31f5b17c9d6d18af728cbdd752aa398ba2ae9356
Amro Madani <33586588+aali1189@users.noreply.github.com>
2023-08-27 19:13:10 +0300

    population.c

---
+++ populations/population.c
@@ -0,0 +1,34 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // TODO: Prompt for start size
+  int start, end;
+  do
+  {
+      start = get_int("Start size: ");
+  }
+  while (start < 9);
+
+    // TODO: Prompt for end size
+  do
+  {
+      end = get_int("End size: ");
+  }
+  while (end <= start);
+
+    // TODO: Calculate number of years until we reach threshold
+  int years, born, died;
+  years = 0;
+
+    while (start < end) {
+        born = start / 3;
+        died = start / 4;
+        start = start + born - died;
+        years++;
+    }
+    // TODO: Print number of years
+    printf("Years: %i\n", years);
+
+}
